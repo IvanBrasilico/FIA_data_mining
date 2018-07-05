6b9f13ef1107c7ed15e05e3705cf6eaf6ff6cd64
Ivan da Silva Brasilico <25052288840@receita.intranet>
2018-07-05 17:18:08 -0300

    Grafico ExD

--- Regressao/ExD.R
+++ Regressao/ExD.R
@@ -3,3 +3,6 @@ ENDIV1 <- read.csv("Regressao/Aula 8/ENDIV1.csv")
 
 ExD <- lm(formula = mb ~ endiv1 + lucrat2 + I(endiv1 ^ 2),  data = ENDIV1)
 summary(ExD)
+plot(mb ~ I(endiv1 ^ 2),  data = ENDIV1)
+abline(ExD)
+
