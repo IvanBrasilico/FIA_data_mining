d678780298c5d63988d8ea5c48e7e3d40d61cb5b
Ivan da Silva Brasilico <25052288840@receita.intranet>
2018-07-20 14:03:57 -0300

    --

--- Regressao/Ex22.R
+++ Regressao/Ex22.R
@@ -10,3 +10,8 @@ summary(Ex22)
 Ex21 <- plm(formula = lwage~educ+black+hisp+exper+expersq+married+union+
               d81+d82+d83+d84+d85+d86+d87, data=WAGEPANEL, model = "random")
 summary(Ex21)
+
+plmtest(Ex21, effect = c("individual"), type = c("bp"))
+
+phtest(Ex21, Ex22)
+help(phtest)
